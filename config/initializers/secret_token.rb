7df54f89a43a40a30ae58fc0c04517c417e50c92
Peter Wang <happypeter1983@gmail.com>
2015-06-18 15:56:01 +0800

    rm junk

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-HaoqiAlipay::Application.config.secret_token = '0992a923504eb3a170aad6f72a7ad29f9646ea6a06994580e770761013711903128c3209f8d6816248264b5e25724e45027ea0bbd62b94d3c5e5390a4e9ab482'
--- db/migrate/20131021025434_add_trade_status_to_orders.rb
+++
@@ -1,5 +0,0 @@
-class AddTradeStatusToOrders < ActiveRecord::Migration
-  def change
-    add_column :orders, :trade_status, :string
-  end
-end
