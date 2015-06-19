b4cc8701600352fc815ca8397daa3d1b7ac2c946
Peter Wang <happypeter1983@gmail.com>
2015-06-19 08:16:22 +0800

    ready for doc

--- app/models/cup.rb
+++
@@ -1,7 +0,0 @@
-class Cup < ActiveRecord::Base
- has_many :orders
-
- def paid?
-   Order.where(trade_status: 'finished', course_id: self.id).first.present?
- end
-end
--- db/migrate/20150618013210_rename_courses_to_cups.rb
+++
@@ -1,5 +0,0 @@
-class RenameCoursesToCups < ActiveRecord::Migration
-  def change
-    rename_table :courses, :cups
-  end
-end
